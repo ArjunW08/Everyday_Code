3a5fd7723a022c752b693872cfbe8425d3ad19b0
ArjunW08 <arjunwankhede08@gmail.com>
2025-08-07 01:07:44 +0530

    Start of DSA

---
+++ DSA/src/BitManipulation/Intro.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+/*
+ * Binary Numbers(Conversion with base 2)
+ * Additions/Subtraction
+ * Bitwise Operators : &, |, ^(XOR), ~, >>, <<
+ * Odd Even Numbers
+ * Swap two Numbers.
+ */
+
+public class Intro {
+
+}
---
+++ DSA/src/TimeComplexity/Intro.java
@@ -0,0 +1,30 @@
+package TimeComplexity;
+
+/*
+ * Time taken by an algorithm as a function of the
+ * length of inputs.
+ * 
+ * Big O Notation
+ * We have to find upper bound and ignore 
+ * smaller bounds and constants.
+ * 
+ * Tree Method to find time complexity : 
+ * Ex: From function we can create a equation like this 
+ * T(n) =  2T(n/2) + nc and calculate the height of tree.
+ * 
+ * 10^8 Operations rule : 
+ * Length(N)            Time Complexity
+ * <=[10-11]            O(n!), O(n^6)
+ * <=[15-18]            O(2^n x n^2)
+ * <100                 O(n^4)
+ * <400                 O(n^3)
+ * <2000                O(n^2 x log n)
+ * <10^4                O(n^2)
+ * <10^6                O(n x log n)
+ * <10^8                O(n), O(log n)
+ * 
+ */
+
+public class Intro {
+
+}
