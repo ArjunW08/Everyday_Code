8d7faead77dd6cbd240959daaf3cc366a7a5cc1d
ArjunW08 <arjunwankhede08@gmail.com>
2025-08-15 01:56:30 +0530

    Bit Manipulation

--- DSA/src/BitManipulation/Intro.java
+++ DSA/src/BitManipulation/Intro.java
@@ -6,6 +6,16 @@ package BitManipulation;
  * Bitwise Operators : &, |, ^(XOR), ~, >>, <<
  * Odd Even Numbers
  * Swap two Numbers.
+ * Bit Masking (1 << i or num >> i)
+ * Find, Set, Clear ith bit using right and left shift 
+ * operation followed by and or OR operation.
+ * Find number of bits to change to convert a to b.
+ * 
+ * XOR : ^ => a ^ a = 0;
+ * 
+ * 1. Find the number in the array which is not repeated twice using XOR
+ * 2. 
+ * 
  */
 
 public class Intro {
